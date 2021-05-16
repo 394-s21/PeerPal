4958ea70eb7a3ee2063687d55aa451589383e96e
Noah Schaffer <noahschaffer2022@u.northwestern.edu>
2021-05-16 13:22:13 -0500

    Fixed package-lock

--- screens/CourseScreen.js
+++ screens/CourseScreen.js
@@ -10,11 +10,8 @@ import RankCard from '../components/RankCard';
 import AssignmentCard from '../components/AssignmentCard'
 import Table from '@material-ui/core/Table'
 import { DataGrid } from '@material-ui/data-grid'
-<<<<<<< HEAD
 import Button from '@material-ui/core/Button';
-=======
 import ClassChart from '../components/ClassChart'
->>>>>>> 654f2a9826c5b2fa9826280cce05f97ebaceb892
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
@@ -115,16 +112,7 @@ const CourseScreen = ({route,navigation}) => {
             </View>
             <View style={{ height: '450', width: '100%' }}>
             <DataGrid rows={rows} columns={columns} pageSize={6} checkboxSelection  />
-<<<<<<< HEAD
-            </div>
-            <Grid item xs={12}>
-              <Button variant="contained" color="primary" size="medium" onClick={() =>  navigation.navigate('UserCourseScreen')}>
-                      Back
-              </Button>
-            </Grid>
-=======
             </View>
->>>>>>> 654f2a9826c5b2fa9826280cce05f97ebaceb892
         </Grid>
     );
 }
