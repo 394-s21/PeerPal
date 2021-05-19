c3b70851bf68a32ac7a71d135663a25e799efcea
Christopher Kapic <christopherkapic@gmail.com>
2021-05-19 12:06:11 -0500

    function working (and returning)

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,12 @@ const fetch = require('node-fetch')
 const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
+const serviceAccount = require("../peerpal-a286b-firebase-adminsdk-f78tm-c4450f23b8.json");
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://peerpal-a286b-default-rtdb.firebaseio.com"
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -15,15 +19,23 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
-exports.getClasses = functions.https.onRequest((req, res) => {
-    const key = req.header.key;
-    fetch('https://canvas.northwestern.edu/api/v1/courses/', {
+exports.getClasses = functions.https.onRequest(async (req, res) => {
+    const authorization = req.headers.authorization;
+    const key = authorization.replace("Bearer ", "");
+    const result = await fetch('https://canvas.northwestern.edu/api/v1/courses/', {
         headers: {
             authorization: `Bearer ${key}`
         }
     })
-    .then(res => res.json())
+    const result_json = await result.json();
 
-    //this is not a function have to import library
-    .then(res => res.send(res));
+    const user_id = result_json[0].enrollments[0].user_id;
+    const classes_result = await fetch(`https://canvas.northwestern.edu/api/v1/users/${user_id}/enrollments`, {
+        headers: {
+            authorization: `Bearer ${key}`
+        }
+    })
+    const classes_result_json = await classes_result.json();
+
+    res.send(classes_result_json);
 })
