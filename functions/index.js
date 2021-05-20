6b4722fb69bf2fd86f7a6bb9dcedfa7b056b798a
iamalanxue <alanxue43@gmail.com>
2021-05-19 19:39:56 -0500

    added initial code for filter current courses

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
-const serviceAccount = require("../peerpal-a286b-firebase-adminsdk-f78tm-c4450f23b8.json");
+const serviceAccount = require("../.firebase/peerpal-a286b-firebase-adminsdk-f78tm-c4450f23b8.json");
 const admin = require('firebase-admin');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -21,21 +21,27 @@ admin.initializeApp({
 
 exports.getClasses = functions.https.onRequest(async (req, res) => {
     const authorization = req.headers.authorization;
+    const per_page = 100; 
     const key = authorization.replace("Bearer ", "");
-    const result = await fetch('https://canvas.northwestern.edu/api/v1/courses/', {
+    const result = await fetch(`https://canvas.northwestern.edu/api/v1/courses/?per_page=${per_page}`, {
         headers: {
             authorization: `Bearer ${key}`
         }
     })
+    const unixTimeZero = Date.parse("2021-06-26T05:00:00Z");
     const result_json = await result.json();
-
-    const user_id = result_json[0].enrollments[0].user_id;
-    const classes_result = await fetch(`https://canvas.northwestern.edu/api/v1/users/${user_id}/enrollments`, {
-        headers: {
-            authorization: `Bearer ${key}`
-        }
+    const currentClasses = result_json.filter(i => {
+        i.end_at == unixTimeZero
     })
-    const classes_result_json = await classes_result.json();
+    // console.log(currentClasses)
+    // const user_id = result_json[0].enrollments[0].user_id;
+    // const classes_result = await fetch(`https://canvas.northwestern.edu/api/v1/users/${user_id}/enrollments?per_page=100`, {
+    //     headers: {
+    //         authorization: `Bearer ${key}`
+    //     }
+    // })
+    // const classes_result_json = await classes_result.json();
+    // console.log(classes_result_json)
 
-    res.send(classes_result_json);
+    res.send(currentClasses);
 })
