31304cd55f37157e2aef9fd69d430b177be4e592
Christopher Kapic <christopherkapic@gmail.com>
2021-05-22 21:04:50 -0500

    got promises

--- functions/index.js
+++ functions/index.js
@@ -35,25 +35,76 @@ exports.getClasses = functions.https.onRequest(async (req, res) => {
             // console.log(i.end_at)
             return i.end_at === "2021-06-26T05:00:00Z"
         })
-        // console.log(currentClasses)
-        
-        // const classes_result = await fetch(`https://canvas.northwestern.edu/api/v1/users/${user_id}/enrollments?per_page=100`, {
-        //     headers: {
-        //         authorization: `Bearer ${key}`
-        //     }
-        // })
-        // const classes_result_json = await classes_result.json();
-        // console.log(classes_result_json)
 
+        // async function loadTranscripts(videoIds){
+        //     // Returns an array of promises
+        //     let to_return = []
+        //     videoIds.forEach(
+        //         (videoId) => {
+        //             if (videoId == null) {
+        //                 to_return.push(null)
+        //             } else {
+        //             to_return.push(
+        //                 fetch(`https://subtitles-for-youtube.p.rapidapi.com/subtitles/${videoId}`, {
+        //                     "method": "GET",
+        //                     "headers": {
+        //                         "x-rapidapi-key": RAPIDAPI_KEY,
+        //                         "x-rapidapi-host": "subtitles-for-youtube.p.rapidapi.com"
+        //                     }
+        //                     }
+        //                 )
+        //             )
+        //             }
+        //         }
+        //     )
+        //     return to_return;
+        // }
         // TODO
         // Call updateGrades on classes (if we want to update every class)
 
         const user_id = result_json[0].enrollments[0].user_id;
-        const updateClasses = async (user_key, user_id, curr_classes) => {
-            
+        const updateClass = async (user_key, user_id, curr_class) => {
+            const assignments = await fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class}/assignments`, {
+                headers: {
+                    authorization: `Bearer ${user_key}`
+                }
+            })
+            const assignments_json = await assignments.json();
+            const quizzes = await fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class}/quizzes`, {
+                headers: {
+                    authorization: `Bearer ${user_key}`
+                }
+            })
+            const quizzes_json = await quizzes.json();
+            let assignment_grades_promises = []
+            assignments_json.forEach(
+                assignment => {
+                    assignment_grades_promises.push(
+                        fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class[0]}/assignments/${assignment.id}/submissions/${user_id}`, {
+                            headers: {
+                                authorization: `Bearer ${user_key}`
+                            }
+                        })
+                    )
+                }
+            )
+            console.log(assignment_grades_promises)
+            let quiz_grades_promises = []
+            quizzes_json.forEach(
+                quiz => {
+                    quiz_grades_promises.push(
+                        // quiz.id might not work
+                        fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class[0]}/quizzes/${quiz.id}/submission`, {
+                            headers: {
+                                authorization: `Bearer ${user_key}`
+                            }
+                        })
+                    )
+                }
+            )
         }
         res.send(currentClasses);
-        await updateClasses(key,user_id,currentClasses);
+        await updateClass(key,user_id,currentClasses[0].id);
     });
 })
 
