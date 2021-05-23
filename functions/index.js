927bd325980a68cde238c323ba6b4337670cf9fd
Han Cao <HanCao2021@u.northwestern.edu>
2021-05-22 19:50:25 -0700

    get course

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,8 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://peerpal-a286b-default-rtdb.firebaseio.com"
   });
+const db = admin.database();
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -64,47 +66,93 @@ exports.getClasses = functions.https.onRequest(async (req, res) => {
 
         const user_id = result_json[0].enrollments[0].user_id;
         const updateClass = async (user_key, user_id, curr_class) => {
+            let checkQuiz = true
+            let checkAssignment = true
+            let assignment_grades_promises = []
+            let quiz_grades_promises = []
+
             const assignments = await fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class}/assignments`, {
                 headers: {
                     authorization: `Bearer ${user_key}`
                 }
             })
             const assignments_json = await assignments.json();
+            // Check assignments
+            if (assignments_json.message){
+                checkAssignment = false
+            }
             const quizzes = await fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class}/quizzes`, {
                 headers: {
                     authorization: `Bearer ${user_key}`
                 }
             })
             const quizzes_json = await quizzes.json();
-            let assignment_grades_promises = []
-            assignments_json.forEach(
-                assignment => {
-                    assignment_grades_promises.push(
-                        fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class[0]}/assignments/${assignment.id}/submissions/${user_id}`, {
-                            headers: {
-                                authorization: `Bearer ${user_key}`
-                            }
-                        })
-                    )
+            // Check if quizzes are disabled
+            if (quizzes_json.message){
+                checkQuiz = false
+                console.log("Check if quiz is disabled", checkQuiz)
+            }
+            console.log("Quizzes: ", quizzes_json)
+            if (checkAssignment){
+                assignments_json.forEach(
+                    assignment => {
+                        assignment_grades_promises.push(
+                            fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class[0]}/assignments/${assignment.id}/submissions/${user_id}`, {
+                                headers: {
+                                    authorization: `Bearer ${user_key}`
+                                }
+                            })
+                        )
+                    }
+                )
+                console.log(assignment_grades_promises)
+            }
+            if (checkQuiz){
+                quizzes_json.forEach(
+                    quiz => {
+                        quiz_grades_promises.push(
+                            // quiz.id might not work
+                            fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class[0]}/quizzes/${quiz.id}/submission`, {
+                                headers: {
+                                    authorization: `Bearer ${user_key}`
+                                }
+                            })
+                        )
+                    }
+                )
+            }
+
+            // Resolve promises
+            if (checkAssignment){
+                let assignmentList = []
+                // let assignmentsPoints = []
+                for (i=0; i<assignment_grades_promises.length; i++ ){
+                    const assignment = await assignment_grades_promises[i]
+                    const assignment_json = await assignment.json()
+                    console.log("assignment", assignment_json)
+                    // assignmentsPoints.push(assignments_json[i].points_possible)
+                    assignment_json.score ? assignmentList.push({id: assignments_json[i].id, points_possible: assignments_json[i].points_possible, score: assignment_json.score})
+                    :
+                    assignmentList.push({id: assignments_json[i].id, points_possible: assignments_json[i].points_possible, score: null})
+                    console.log("points possible", assignment_json)
                 }
-            )
-            console.log(assignment_grades_promises)
-            let quiz_grades_promises = []
-            quizzes_json.forEach(
-                quiz => {
-                    quiz_grades_promises.push(
-                        // quiz.id might not work
-                        fetch(`https://canvas.northwestern.edu/api/v1/courses/${curr_class[0]}/quizzes/${quiz.id}/submission`, {
-                            headers: {
-                                authorization: `Bearer ${user_key}`
-                            }
-                        })
-                    )
+                const courseRef = db.ref('/course/' + curr_class[0]);
+                    // console.log("orderRef",orderRef)
+                    courseRef.update({
+                        Assignment: assignmentList
+                });
+            }
+            if (checkQuiz){
+                for (i=0; i<quiz_grades_promises.length; i++ ){
+                    const quiz = await quiz_grades_promises[i]
+                    
                 }
-            )
+            }
         }
         res.send(currentClasses);
         await updateClass(key,user_id,currentClasses[0].id);
+        console.log("Assignment list: ", assignment_grades_promises)
+        
     });
 })
 
