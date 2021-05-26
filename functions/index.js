93b5e83d558eb52e6db8e6eae45cd65d4f60e8b3
Christopher Kapic <christopherkapic@gmail.com>
2021-05-26 11:22:13 -0500

    step 3 prep

--- functions/index.js
+++ functions/index.js
@@ -202,7 +202,7 @@ exports.updateClasses = functions.https.onRequest(async (req, res) => {
         //             }
         //         }))
         // })
-        let courses_assignments_json =await Promise.all(course_ids.map( async(course_id) => fetch(`https://canvas.northwestern.edu/api/v1/courses/${course_id}/assignments?per_page=${per_page}`, {
+        let courses_assignments_json = await Promise.all(course_ids.map( async(course_id) => fetch(`https://canvas.northwestern.edu/api/v1/courses/${course_id}/assignments?per_page=${per_page}`, {
             headers: {
                 authorization: `Bearer ${key}`
             }})))
@@ -216,6 +216,7 @@ exports.updateClasses = functions.https.onRequest(async (req, res) => {
         let courses_assignments_scores = []
         // Updating each course
         courses_assignments_json.map((course_assignments_json, idx) => {
+            let course_assignments_scores = []
             // Updating each assignment
             course_assignments_json.map((assignment_json) => {
                 const assignment_ref = db.ref('/course/' + course_ids[idx] + '/assignments/' + assignment_json.id);
@@ -226,18 +227,22 @@ exports.updateClasses = functions.https.onRequest(async (req, res) => {
                 })
 
                 // Preparing for step 3.
-                // courses_assignments_scores[idx].push(
-                //     fetch(`https://canvas.northwestern.edu/api/v1/courses/${course_ids[idx]}/assignments/${assignment_json.id}/submissions/${user_id}`, {
-                //         headers: {
-                //             authorization: `Bearer ${key}`
-                //         }
-                //     })
-                // );
+                course_assignments_scores.push(
+                    fetch(`https://canvas.northwestern.edu/api/v1/courses/${course_ids[idx]}/assignments/${assignment_json.id}/submissions/${user_id}`, {
+                        headers: {
+                            authorization: `Bearer ${key}`
+                        }
+                    })
+                );
             })
+            courses_assignments_scores.push(course_assignments_scores)
         });
+
+        console.log(courses_assignments_scores);
 
 
         // 3. For each class, for each assignment, get score, set score in database
+        let courses_assignments_scores_json;
         // let courses_assignments_scores_json = [];
         // courses_assignments_scores.map((course_assignment_scores) => {
         //     Promise.all(course_assignment_scores)
@@ -257,8 +262,7 @@ exports.updateClasses = functions.https.onRequest(async (req, res) => {
         // })
 
         // 4. For each class, get enrollment_score, set enrollment_score in database
-        console.log(courses_assignments_json)
-        
+        res.send('Hello there. \n General Kenobi!')
     });
 });
 
