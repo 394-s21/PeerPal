5d08b2d03ed3dee229b8925c4092fa3f29b44432
bcisaac <ibconner01@gmail.com>
2021-05-27 09:52:42 -0500

    usercoursetable and class rechart

---
+++ Components/UserCourseTable.js
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { DataGrid } from '@material-ui/data-grid';
+import { firebase } from './firebase';
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+      padding: 10
+    },
+    table:{
+    },
+    pos: {
+      marginBottom: 12,
+    },
+});
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90},
+    { field: 'assignment_name', headerName: 'Assignment', width: 250 },
+    { field: 'score', headerName: 'Score', type: 'number', width: 90},
+    { field: 'points_possible', headerName: 'Total', type: 'number', width: 90 },
+    { field: 'percentage', headerName: 'Percentage', type: 'number', width: 90,
+      valueGetter: (params) =>
+      params.getValue(params.id, 'score')/params.getValue(params.id, 'points_possible')
+      },
+    { field: 'strats', headerName: 'Learning Strategies', width: 150, sortable: false },
+];
+
+const update_rows = () => {
+    // initializes rows for our datagrid
+    // finds all the users that will be our rows
+    const rows = [];
+    const course_users = [];
+    Object.values(courseInfo.enrollemnt_scores).map((user_id) => {
+    course_users.push(user_id)
+    })
+    setCourseUsers(course_users)
+    // adds all important data to the rows
+    Object.entries(courseInfo.assignments).map((assignment, i) => {
+    rows.push({ id: course_users[i],
+                assignment_name: assignment.assignment_name,
+                score: assignment.users.course_users[i].score,
+                points_possible: assignment.points_possible,
+                stats: ''
+                })
+    })
+    setRows(rows)
+}
+
+const UserCourseTable = ({route,navigation}) => {
+
+    const classes = useStyles();
+
+    // updates courseInfo state with firebase json and updates rows
+    const [rows, setRows] = useState([])
+    const [courseUsers, setCourseUsers] = useState([])
+    const [courseInfo, setCourseInfo] = useState({})
+    useEffect(() => {
+    const cdb = firebase.database().ref('course/137169/');
+    const handleData = snap => {
+        if (snap.val()) {setCourseInfo(snap.val());
+                        update_rows();};
+    };
+    cdb.on('value', handleData, error =>(error));
+    return () => { db.off('value', handleData);};
+    }, []);
+
+    return (
+        <DataGrid rows={rows} columns={columns} pageSize={6}/>
+    );
+}
+
+export default UserCourseTable; 
--- screens/CourseScreen.js
+++ screens/CourseScreen.js
@@ -6,13 +6,16 @@ import Grid from '@material-ui/core/Grid'
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography'
-import RankCard from '../components/RankCard';
-import AssignmentCard from '../components/AssignmentCard'
+import RankCard from '../Components/RankCard';
+import AssignmentCard from '../Components/AssignmentCard'
 import Table from '@material-ui/core/Table'
 import { DataGrid } from '@material-ui/data-grid'
 import Button from '@material-ui/core/Button';
-import ClassChart from '../components/ClassChart'
-import { firebase } from '../components/firebase';
+import { firebase } from '../Components/firebase';
+import { object } from 'firebase-functions/lib/providers/storage';
+import ClassChart from '../Components/ClassChart'
+import UserCourseTable from '../Components/UserCourseTable'
+import ClassRechart from '../Components/ClassRechart'
 
 const useStyles = makeStyles({
     root: {
@@ -20,8 +23,8 @@ const useStyles = makeStyles({
       padding: 10
     },
     title: {
-      fontSize: 16,
-      textAlign: 'center'
+      width: '100%',
+      alignItems: 'center'
     },
     scoreCard:{
         padding:5
@@ -33,58 +36,17 @@ const useStyles = makeStyles({
         textAlign: 'center'
     },
     table:{
-      
-
+      height: '450', 
+      width: '100%'
     },
     pos: {
       marginBottom: 12,
     },
 });
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90},
-  { field: 'assignment_name', headerName: 'Assignment', width: 250 },
-  { field: 'score', headerName: 'Score', type: 'number', width: 90},
-  { field: 'points_possible', headerName: 'Total', type: 'number', width: 90 },
-  { field: 'percentage', headerName: 'Percentage', type: 'number', width: 90,
-    valueGetter: (params) =>
-    params.getValue(params.id, 'score')/params.getValue(params.id, 'points_possible')
-    },
-  { field: 'strats', headerName: 'Learning Strategies', width: 150, sortable: false },
-];
-
-// updates courseInfo state with firebase json
-[courseInfo, setCourseInfo] = useState({})
-useEffect(() => {
-  const cdb = firebase.database().ref('course/137169');
-  const handleData = snap => {
-    if (snap.val()) setCourseInfo(snap.val());
-  };
-  cdb.on('value', handleData, error =>(error));
-  return () => { db.off('value', handleData);};
-}, []);
-
-// makes rows for our datagrid
-const rows = [];
-
-// need to push rows with the data we need
-// the folllowing code is an artifact of a failed attempt T_T
-course.values(this.props.a).map((item,idx) => {
-  rows.push({ id: item.users[`${users.idx}`], 
-              assignment_name: item.assignment_name,
-              score: item.users[`${users.idx}`].score,
-              states: ''
-            })
-  })
-
 const CourseScreen = ({route,navigation}) => {
 
-  const ref = firebase.database().ref('/course/137169/Assignments/0')
-  console.log(ref)
-
-
     const classes = useStyles();
-
     const getScore = (info, weights, keys, studentIDX) => {
       let to_return = 0;
       keys.map((item, idx) => {
@@ -94,18 +56,21 @@ const CourseScreen = ({route,navigation}) => {
     }
 
     return (
-        <Grid className={classes.root} container direction="row"
+        <Grid container
+        className={classes.root}
+        direction="row"
         justify="center"
         alignItems="top">
-            <RankCard score={getScoree(rows, rowWeight, keys, 0)} type={'Personal'}/>
-            <RankCard score={90} type={'Class'}/>
-            <View style={{width: '100%', alignItems: 'center'}}>
+            {/* <RankCard score={getScore(rows, rowWeight, keys, 0)} type={'Personal'}/>
+            <RankCard score={90} type={'Class'}/> */}
+            <Grid item className={classes.title}>
               <Typography>Class Performance</Typography>
               <ClassChart data={'hi'}/>
-            </View>
-            <View style={{ height: '450', width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} pageSize={6} checkboxSelection  />
-            </View>
+             {/* <ClassChart data={'hi'}/> */}
+            </Grid>
+            <Grid item className={classes.table}>
+              <UserCourseTable/>
+            </Grid>
         </Grid>
     );
 }
