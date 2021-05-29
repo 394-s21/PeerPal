a5c0f583375cdf6425f79b2e56a4aa82a5d443f8
Christopher Kapic <christopherkapic@gmail.com>
2021-05-29 18:56:43 -0500

    Populating chart with Fetch

--- components/ClassChart.js
+++ components/ClassChart.js
@@ -1,3 +1,4 @@
+import { Typography } from '@material-ui/core';
 import React, {useState} from 'react';
 // import {View} from 'react-native'
 // import { makeStyles } from '@material-ui/core/styles';
@@ -13,67 +14,54 @@ import { Chart, VerticalAxis, HorizontalAxis, Line, Area } from 'react-native-re
 
 
 const ClassChart = (props) => {
-    console.log('props.course', props.course)
-    let assignments = props.course ? Object.entries(props.course.assignments) : ''
-    console.log('assignments', assignments)
 
-    return (
-        <Chart
+    if (props.course) {
+        const num_assignments = Object.keys(props.course.assignments).length
+
+        let user_assignments = []
+
+        Object.entries(props.course.assignments).map((assignment, idx) => {
+            if (assignment[1].users["114588"].score !== "no_score") {
+                user_assignments.push({x: (idx+1), y: ( 100 * assignment[1].users["114588"].score / assignment[1].points_possible)})
+            }
+        })
+
+        let other_users = [[], [], [], [], [], []]
+
+        Object.entries(props.course.assignments).map((assignment, idx) => {
+            Object.entries(assignment[1].users).map((user, idxx) => {
+                if (user[1].score !== "no_score") {
+                    other_users[idxx].push({x: idx, y: ( 100 * user[1].score / assignment[1].points_possible)})
+                }
+            })
+        })
+
+        console.log('user_assignments', user_assignments)
+        return (
+            <Chart
         style={{ height: 400, width: 800 }}
         padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
-        xDomain={{ min: 1, max: 4 }}
-        yDomain={{ min: 50, max: 110 }}
+        xDomain={{ min: 1, max: (num_assignments + 1) }}
+        yDomain={{ min: 0, max: 110 }}
         >
         <VerticalAxis tickCount={10} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
-        <HorizontalAxis tickCount={3} />
-        <Area theme={{ gradient: { from: { color: '#44bd32', opacity: 0.4 }, to: { color: '#44bd32', opacity: 0.2 } }}} data={
-            [
-                // a1: 100, a2: 95, a3: 85, a4: 70
-                { x: 1, y: 100 },
-                { x: 2, y: 95 },
-                { x: 3, y: 85 },
-                { x: 4, y: 70 }
-            ]} />
-        <Line theme={{ stroke: { color: '#40a992', width: 2 }, scatter: { default: { width: 8, height: 8, rx: 4, color: '#40a992' }, selected: { color: 'red' } } }}  data={
-            [
-                // a1: 100, a2: 100, a3: 100, a4: 88 
-                { x: 1, y: 100 },
-                { x: 2, y: 100 },
-                { x: 3, y: 100 },
-                { x: 4, y: 88 }
-            ]
-        }/>
-        <Line theme={{ stroke: { color: '#40a992', width: 2 }, scatter: { default: { width: 8, height: 8, rx: 4, color: '#40a992' }, selected: { color: 'red' } } }}  data={
-            [
-                // a1: 95, a2: 85, a3: 95, a4: 90
-                { x: 1, y: 95 },
-                { x: 2, y: 85 },
-                { x: 3, y: 95 },
-                { x: 4, y: 90 }
-            ]
-        }/>
-        <Line theme={{ stroke: { color: '#40a992', width: 2 }, scatter: { default: { width: 8, height: 8, rx: 4, color: '#40a992' }, selected: { color: 'red' } } }}  data={
-            [
-                // a1: 65, a2: 75, a3: 95, a4: 72
-                // a1: 75, a2: 75, a3: 75, a4: 62 
-                // a1: 75, a2: 85, a3: 85, a4: 76 
-                { x: 1, y: 65 },
-                { x: 2, y: 75 },
-                { x: 3, y: 95 },
-                { x: 4, y: 72 }
-            ]
-        }/>
+        <HorizontalAxis tickCount={num_assignments + 1} />
+        <Area theme={{ gradient: { from: { color: '#44bd32', opacity: 0.4 }, to: { color: '#44bd32', opacity: 0.2 } }}} data={user_assignments} />
+        {
+            other_users.map((user, idx) => {
+                <Line theme={{ stroke: { color: '#40a992', width: 2 }, scatter: { default: { width: 8, height: 8, rx: 4, color: '#40a992' }, selected: { color: 'red' } } }}  data={user} nothing={console.log('user', user)}/>
+            })
+        }
+        
         
         <Line theme={{ stroke: { color: '#54bd42', width: 5 }, scatter: { default: { width: 8, height: 8, rx: 4, color: '#54bd42' }, selected: { color: 'red' } } }}  data={
-            [
-                // a1: 100, a2: 95, a3: 85, a4: 70
-                { x: 1, y: 100 },
-                { x: 2, y: 95 },
-                { x: 3, y: 85 },
-                { x: 4, y: 70 }
-            ]
+            user_assignments
         }/>
         </Chart>
+        )
+    }
+    return (
+        <Typography>Loading...</Typography>
     )
 }
 
